1cb30ac1983dc7ba022a9e13c15c0ce3cb4a52ab
ssdvd-labs <david.santana98@hotmail.com>
2023-06-28 23:32:30 -0300

    subindo tudo

---
+++ pratico/iac/main.tf
@@ -0,0 +1,34 @@
+# Selecionando o provedor cloud
+terraform {
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 3.27"
+        }
+    }
+
+    required_version = ">= 0.14.9"
+}
+
+# Configurando o provedor
+provider "aws" {
+    profile = "default"
+    region  = "us-east-1"
+}
+
+# Selecionando os recursos
+resource "aws_instance" "app_server" {
+    ami             = "ami-0261755bbcb8c4a84"
+    instance_type   = "t2.micro"
+    #definindo o par de chave
+    key_name = "IaC-ssdvd"
+    user_data = <<-EOF
+                   #!/bin/bash
+                   cd /home/ubuntu
+                   echo "<h1>Feito com o Terraform</h1>" > index.html
+                   nohup busybox httpd -f -p 8080 &
+                   EOF
+    tags = {
+        name = "Teste AWS"
+    }
+}
